dde01d2d7bff91fda391b4161b44258698c21338
Daniel Lemire <lemire@gmail.com>
2016-05-04 12:12:11 -0400

    Adding more tests

--- include/conciseutil.h
+++ include/conciseutil.h
@@ -15,23 +15,8 @@ static inline int32_t min(T x, T y) {
 
 /**
  * The highest representable integer.
- * <p>
- * Its value is computed as follows. The number of bits required to
- * represent the longest sequence of 0's or 1's is
- * <tt>ceil(log<sub>2</sub>(({@link Integer#MAX_VALUE} - 31) / 31)) = 27</tt>.
- * Indeed, at least one literal exists, and the other bits may all be 0's or
- * 1's, that is <tt>{@link Integer#MAX_VALUE} - 31</tt>. If we use:
- * <ul>
- * <li> 2 bits for the sequence type;
- * <li> 5 bits to indicate which bit is set;
- * </ul>
- * then <tt>32 - 5 - 2 = 25</tt> is the number of available bits to
- * represent the maximum sequence of 0's and 1's. Thus, the maximal bit that
- * can be set is represented by a number of 0's equals to
- * <tt>31 * (1 << 25)</tt>, followed by a literal with 30 0's and the
- * MSB (31<sup>st</sup> bit) equal to 1
  */
-constexpr static int32_t  MAX_ALLOWED_INTEGER = 31 * (INT32_C(1) << 25) + 30; // 1040187422
+constexpr static uint32_t  MAX_ALLOWED_INTEGER = 31 * (UINT32_C(1) << 25) + 30; // 1040187422
 
 
 /**
@@ -58,19 +43,9 @@ constexpr static uint32_t ALL_ONES_WITHOUT_MSB = UINT32_C(0x7FFFFFFF);
  * Sequence bit
  */
 constexpr static uint32_t SEQUENCE_BIT = UINT32_C(0x40000000);
+
 /**
- * Calculates the modulus division by 31 in a faster way than using <code>n % 31</code>
- * <p>
- * This method of finding modulus division by an integer that is one less
- * than a power of 2 takes at most <tt>O(lg(32))</tt> time. The number of operations
- * is at most <tt>12 + 9 * ceil(lg(32))</tt>.
- * <p>
- * See <a
- * href="http://graphics.stanford.edu/~seander/bithacks.html">http://graphics.stanford.edu/~seander/bithacks.html</a>
- *
- * @param n
- *            number to divide
- * @return <code>n % 31</code>
+ * Calculates the modulus division by 31 in a faster way than using n % 31
  */
 static inline uint32_t maxLiteralLengthModulus(uint32_t n) {
     uint32_t m = (n & UINT32_C(0xC1F07C1F)) + ((n >> 5) & UINT32_C(0xC1F07C1F));
@@ -96,11 +71,7 @@ static inline uint32_t maxLiteralLengthModulus(uint32_t n) {
 
 
 /**
- * Calculates the multiplication by 31 in a faster way than using <code>n * 31</code>
- *
- * @param n
- *            number to multiply
- * @return <code>n * 31</code>
+ * Calculates the multiplication by 31 in a faster way than using n * 31
  */
 static inline uint32_t maxLiteralLengthMultiplication(uint32_t n) {
     return (n << 5) - n;
@@ -108,10 +79,6 @@ static inline uint32_t maxLiteralLengthMultiplication(uint32_t n) {
 
 /**
  * Calculates the division by 31
- *
- * @param n
- *            number to divide
- * @return <code>n / 31</code>
  */
 static inline uint32_t maxLiteralLengthDivision(uint32_t n) {
     return n / 31;
@@ -122,10 +89,6 @@ static inline uint32_t maxLiteralLengthDivision(uint32_t n) {
 
 /**
  * Checks whether a word is a literal one
- *
- * @param word
- *            word to check
- * @return <code>true</code> if the given word is a literal word
  */
 static inline bool isLiteral(uint32_t word) {
     // "word" must be 1*
@@ -135,10 +98,6 @@ static inline bool isLiteral(uint32_t word) {
 
 /**
  * Checks whether a word contains a sequence of 1's
- *
- * @param word
- *            word to check
- * @return <code>true</code> if the given word is a sequence of 1's
  */
 static inline bool isOneSequence(uint32_t word) {
     // "word" must be 01*
@@ -147,10 +106,6 @@ static inline bool isOneSequence(uint32_t word) {
 
 /**
  * Checks whether a word contains a sequence of 0's
- *
- * @param word
- *            word to check
- * @return <code>true</code> if the given word is a sequence of 0's
  */
 static inline bool isZeroSequence(uint32_t word) {
     // "word" must be 00*
@@ -160,14 +115,6 @@ static inline bool isZeroSequence(uint32_t word) {
 /**
  * Checks whether a word contains a sequence of 0's with no set bit, or 1's
  * with no unset bit.
- * <p>
- * <b>NOTE:</b> when {@link #simulateWAH} is <code>true</code>, it is
- * equivalent to (and as fast as) <code>!</code>{@link #isLiteral(int)}
- *
- * @param word
- *            word to check
- * @return <code>true</code> if the given word is a sequence of 0's or 1's
- *         but with no (un)set bit
  */
 static inline bool isSequenceWithNoBits(uint32_t word) {
     // "word" must be 0?00000*
@@ -176,36 +123,22 @@ static inline bool isSequenceWithNoBits(uint32_t word) {
 
 /**
  * Gets the number of blocks of 1's or 0's stored in a sequence word
- *
- * @param word
- *            word to check
- * @return the number of blocks that follow the first block of 31 bits
  */
-static inline int getSequenceCount(uint32_t word) {
+static inline uint32_t getSequenceCount(uint32_t word) {
     // get the 25 LSB bits
     return word & UINT32_C(0x01FFFFFF);
 }
 
 /**
  * Clears the (un)set bit in a sequence
- *
- * @param word
- *            word to check
- * @return the sequence corresponding to the given sequence and with no
- *         (un)set bits
  */
 static inline uint32_t getSequenceWithNoBits(uint32_t word) {
     // clear 29 to 25 LSB bits
     return (word & UINT32_C(0xC1FFFFFF));
 }
 /**
- * Returns <code>true</code> when the given 31-bit literal string (namely,
+ * Returns true when the given 31-bit literal string (namely,
  * with MSB set) contains only one set bit
- *
- * @param literal
- *            literal word (namely, with MSB unset)
- * @return <code>true</code> when the given literal contains only one set
- *         bit
  */
 static inline bool containsOnlyOneBit(uint32_t literal) {
     return (literal & (literal - 1)) == 0;
@@ -215,14 +148,6 @@ static inline bool containsOnlyOneBit(uint32_t literal) {
 /**
  * Gets the position of the flipped bit within a sequence word. If the
  * sequence has no set/unset bit, returns -1.
- * <p>
- * Note that the parameter <i>must</i> a sequence word, otherwise the
- * result is meaningless.
- *
- * @param word
- *            sequence word to check
- * @return the position of the set bit, from 0 to 31. If the sequence has no
- *         set/unset bit, returns -1.
  */
 static inline int getFlippedBit(uint32_t word) {
     // get bits from 30 to 26
@@ -232,19 +157,12 @@ static inline int getFlippedBit(uint32_t word) {
 
 /**
  * Gets the bits contained within the literal word
- *
- * @param word literal word
- * @return the literal word with the most significant bit cleared
  */
-static inline int getLiteralBits(uint32_t word) {
+static inline uint32_t getLiteralBits(uint32_t word) {
     return ALL_ONES_WITHOUT_MSB & word;
 }
 /**
  * Gets the number of set bits within the literal word
- *
- * @param word
- *            literal word
- * @return the number of set bits within the literal word
  */
 static inline int getLiteralBitCount(uint32_t word) {
     return __builtin_popcount(getLiteralBits(word));
--- tests/unit.cpp
+++ tests/unit.cpp
@@ -1,48 +1,200 @@
 #include <iostream>
 #include <cassert>
+#include <set>
 
 #include "concise.h"
 
-template <bool ewahmode>
+template<bool ewahmode>
 void basictest() {
-    ConciseSet<ewahmode> test1;
-    test1.add(1);
-    assert(test1.contains(1));
-    test1.add(2);
-    assert(test1.contains(2));
-    test1.add(3);
-    assert(test1.contains(3));
-    test1.add(100);
-    assert(test1.contains(100));
-    test1.add(1000);
-    assert(test1.contains(1000));
-    assert(test1.size() == 5);
-    ConciseSet<ewahmode> test2;
-    test2.add(0);
-    assert(test2.contains(0));
-    test2.add(2);
-    assert(test2.contains(2));
-    test2.add(3);
-    assert(test2.contains(3));
-    test2.add(100);
-    assert(test2.contains(100));
-    test2.add(3000);
-    assert(test2.contains(3000));
-    assert(test2.size() == 5);
-    ConciseSet<ewahmode> tmp;
-    tmp = test1.logicalor(test2);
-    assert(tmp.size() == 7);
-    tmp = test1.logicaland(test2);
-    assert(tmp.size() == 3);
-    tmp.add(100000);
-    assert(tmp.size() == 4);
-    tmp.shrink_to_fit();
- }
+	std::cout << __PRETTY_FUNCTION__ << std::endl;
+	ConciseSet<ewahmode> test1;
+	test1.add(1);
+	assert(test1.contains(1));
+	test1.add(2);
+	assert(test1.contains(2));
+	test1.add(3);
+	assert(test1.contains(3));
+	test1.add(100);
+	assert(test1.contains(100));
+	test1.add(1000);
+	assert(test1.contains(1000));
+	assert(test1.size() == 5);
+	ConciseSet<ewahmode> test2;
+	test2.add(0);
+	assert(test2.contains(0));
+	test2.add(2);
+	assert(test2.contains(2));
+	test2.add(3);
+	assert(test2.contains(3));
+	test2.add(100);
+	assert(test2.contains(100));
+	test2.add(3000);
+	assert(test2.contains(3000));
+	assert(test2.size() == 5);
+	ConciseSet<ewahmode> tmp;
+	tmp = test1.logicalor(test2);
+	assert(tmp.size() == 7);
+	tmp = test1.logicaland(test2);
+	assert(tmp.size() == 3);
+	tmp.add(100000);
+	assert(tmp.size() == 4);
+	tmp.shrink_to_fit();
+}
+
+template<bool ewahmode>
+void longtest() {
+	std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+	ConciseSet<ewahmode> testc;
+	for (int k = 0; k < 1000; ++k) {
+		testc.add(k * 2);
+		testc.add(k * 2 + 1);
+	}
+	assert(testc.size() == 2000);
+	for (int k = 0; k < 1000; ++k) {
+		assert(testc.contains(k * 2));
+		assert(testc.contains(k * 2 + 1));
+	}
+	ConciseSet<ewahmode> test1;
+	for (int k = 0; k < 1000; ++k) {
+		test1.add(k * 2);
+	}
+	for (int k = 0; k < 1000; ++k) {
+		assert(test1.contains(k * 2));
+		assert(!test1.contains(k * 2 + 1));
+	}
+	assert(test1.size() == 1000);
+	ConciseSet<ewahmode> shouldbetest1;
+	shouldbetest1 = testc.logicaland(test1);
+	assert(shouldbetest1.size() == 1000);
+	for (int k = 0; k < 1000; ++k) {
+		assert(shouldbetest1.contains(k * 2));
+		assert(!shouldbetest1.contains(k * 2 + 1));
+	}
+	ConciseSet<ewahmode> test2;
+	for (int k = 0; k < 1000; ++k) {
+		test2.add(k * 2 + 1);
+	}
+	for (int k = 0; k < 1000; ++k) {
+		assert(!test2.contains(k * 2));
+		assert(test2.contains(k * 2 + 1));
+	}
+	assert(test2.size() == 1000);
+
+	ConciseSet<ewahmode> tmp;
+	tmp = test1.logicalor(test2);
+	assert(tmp.size() == 2000);
+	for (int k = 0; k < 1000; ++k) {
+		assert(tmp.contains(k * 2));
+		assert(tmp.contains(k * 2 + 1));
+	}
+	tmp = tmp.logicaland(test2);
+	assert(tmp.size() == 1000);
+
+	tmp = test1.logicaland(test2);
+	assert(tmp.size() == 0);
+}
+
+static std::set<uint32_t> unite(std::set<uint32_t> s1, std::set<uint32_t> s2) {
+	std::set < uint32_t > answer;
+	for (std::set<uint32_t>::iterator i = s1.begin(); i != s1.end(); i++) {
+		answer.insert(*i);
+	}
+	for (std::set<uint32_t>::iterator i = s2.begin(); i != s2.end(); i++) {
+		answer.insert(*i);
+	}
+	return answer;
+}
+
+static std::set<uint32_t> intersect(std::set<uint32_t> s1,
+		std::set<uint32_t> s2) {
+	std::set < uint32_t > answer;
+	for (std::set<uint32_t>::iterator i = s1.begin(); i != s1.end(); i++) {
+		if (s2.find(*i) != s2.end()) {
+			answer.insert(*i);
+		}
+	}
+	return answer;
+}
+
+template<bool ewahmode>
+static bool equals(std::set<uint32_t> s, ConciseSet<ewahmode> c) {
+	if (s.size() != c.size())
+		return false;
+	for (std::set<uint32_t>::iterator i = s.begin(); i != s.end(); i++) {
+		if (!c.contains(*i))
+			return false;
+	}
+	return true;
+}
+
+template<bool ewahmode>
+void variedtest() {
+
+	std::cout << __PRETTY_FUNCTION__ << std::endl;
+
+	ConciseSet<ewahmode> test1;
+	std::set < uint32_t > set1;
+
+	for (int k = 0; k < 1000; ++k) {
+		test1.add(k);
+		set1.insert(k);
+	}
+	for (int k = 1000; k < 2000; k += 2) {
+		test1.add(k);
+		set1.insert(k);
+	}
+	for (int k = 5000; k < 6000; k += 1) {
+		test1.add(k);
+		set1.insert(k);
+	}
+	for (int k = 6500; k < 8000; k += 1) {
+		test1.add(k);
+		set1.insert(k);
+	}
+
+	ConciseSet<ewahmode> test2;
+	std::set < uint32_t > set2;
+	for (int k = 0; k < 1100; k += 3) {
+		test2.add(k);
+		set2.insert(k);
+	}
+	for (int k = 1100; k < 2000; ++k) {
+		test2.add(k);
+		set2.insert(k);
+	}
+	for (int k = 5500; k < 6500; ++k) {
+		test2.add(k);
+		set2.insert(k);
+	}
+	for (int k = 7500; k < 8100; ++k) {
+		test2.add(k);
+		set2.insert(k);
+	}
+	std::set < uint32_t > trueunion = unite(set1, set2);
+	std::set < uint32_t > trueinter = intersect(set1, set2);
+	ConciseSet<ewahmode> union1;
+	ConciseSet<ewahmode> union2;
+	union1 = test1.logicalor(test2);
+	union2 = test1.logicalor(test2);
+	assert(equals(trueunion, union1));
+	assert(equals(trueunion, union2));
+	ConciseSet<ewahmode> intersect1;
+	ConciseSet<ewahmode> intersect2;
+	intersect1 = test1.logicaland(test2);
+	intersect2 = test1.logicaland(test2);
+	assert(equals(trueinter, intersect1));
+	assert(equals(trueinter, intersect2));
+}
 
 int main() {
-    basictest<true>();
-    basictest<false>();
-    std::cout <<"code might be ok" << std::endl;
+	basictest<true>();
+	basictest<false>();
+	longtest<true>();
+	longtest<false>();
+	variedtest<true>();
+	variedtest<false>();
 
+	std::cout << "code might be ok" << std::endl;
 
 }
